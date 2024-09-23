73848b54c4de754082bf313d31fa9cacc902297e
BRADDTELES <braddteles@gmail.com>
2024-09-23 13:46:06 -0300

    Executando códigos Java

---
+++ AulaAprendaJavaDoZero/app/src/main/java/com/application/aulaaprendajavadozero/MainActivity.java
@@ -0,0 +1,13 @@
+package com.application.aulaaprendajavadozero;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
