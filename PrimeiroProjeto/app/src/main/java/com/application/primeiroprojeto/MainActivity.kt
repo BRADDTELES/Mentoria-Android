940beb517c60cb1d7c19678adc0c0f265fe62efb
BRADDTELES <braddteles@gmail.com>
2024-09-23 09:15:54 -0300

    Criando um projeto e introduzindo ao ConstraintLayout, texto e botão

---
+++ PrimeiroProjeto/app/src/main/java/com/application/primeiroprojeto/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.application.primeiroprojeto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun clique(view: View){
+        println("foi clicado")
+    }
+
+}
