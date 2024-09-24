d74e2b4a228eeee254fe2c85b0e6d151748685c2
BRADDTELES <braddteles@gmail.com>
2024-09-24 07:01:44 -0300

    Finalizando App Frases do dia

--- AulaFrasesDoDia/app/src/main/java/com/application/aulafrasesdodia/MainActivity.java
+++ AulaFrasesDoDia/app/src/main/java/com/application/aulafrasesdodia/MainActivity.java
@@ -2,6 +2,10 @@ package com.application.aulafrasesdodia;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void gerarFraseAleatoria(View view){
+
+        String[] frases = {
+                "Frase 01",
+                "Frase 02",
+                "Frase 03",
+                "Frase 04",
+        };
+
+        int numeroAleatorio = new Random().nextInt(4);//0,1,2,3
+
+        TextView texto = findViewById(R.id.textFrase);
+        texto.setText( frases[numeroAleatorio] );
+
     }
 }
