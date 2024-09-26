69aefe07e94117735bdc3ffd3974f48402933b8f
BRADDTELES <braddteles@gmail.com>
2024-09-26 06:37:49 -0300

    Escolhendo opção - app Pedra papel ou tesoura

--- AulaPedraPapelTesoura/app/src/main/java/com/application/aulapedrapapeltesoura/MainActivity.java
+++ AulaPedraPapelTesoura/app/src/main/java/com/application/aulapedrapapeltesoura/MainActivity.java
@@ -1,24 +1,31 @@
 package com.application.aulapedrapapeltesoura;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
+
+    public void selecionarPedra( View view ){
+        vereficarGanhador("pedra");
+    }
+
+    public void selecionarPapel( View view ){
+        vereficarGanhador("papel");
+    }
+
+    public void selecionarTesoura( View view ){
+        vereficarGanhador("tesoura");
+    }
+
+    private void vereficarGanhador( String escolhaUsuario ){
+        System.out.println("Item clicado: " + escolhaUsuario);
+    }
+
 }
