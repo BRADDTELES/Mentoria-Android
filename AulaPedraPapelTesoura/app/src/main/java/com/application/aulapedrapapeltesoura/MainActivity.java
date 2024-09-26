1e24dbc6e1533350ed73c63ce2fccc85ca29b1bc
BRADDTELES <braddteles@gmail.com>
2024-09-26 06:50:49 -0300

    Escolha aleatória - App Pedra papel ou tesoura

--- AulaPedraPapelTesoura/app/src/main/java/com/application/aulapedrapapeltesoura/MainActivity.java
+++ AulaPedraPapelTesoura/app/src/main/java/com/application/aulapedrapapeltesoura/MainActivity.java
@@ -3,6 +3,9 @@ package com.application.aulapedrapapeltesoura;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,8 +27,39 @@ public class MainActivity extends AppCompatActivity {
         vereficarGanhador("tesoura");
     }
 
-    private void vereficarGanhador( String escolhaUsuario ){
-        System.out.println("Item clicado: " + escolhaUsuario);
+    private String gerarEsolhaaleatoriaApp() {
+
+        String[] opcoes = {
+                "pedra",
+                "papel",
+                "tesoura"
+        };
+        int numeroAleatorio = new Random().nextInt(3);
+
+        ImageView imagemApp = findViewById(R.id.image_app);
+        String escolhaApp = opcoes[numeroAleatorio];
+        switch ( escolhaApp ){
+            case "pedra":
+                imagemApp.setImageResource( R.drawable.pedra );
+                break;
+            case "papel":
+                imagemApp.setImageResource( R.drawable.papel );
+                break;
+            case "tesoura":
+                imagemApp.setImageResource( R.drawable.tesoura );
+                break;
+        }
+        return escolhaApp;
+
     }
+
+    private void vereficarGanhador( String escolhaUsuario ){
+
+        String escolhaApp = gerarEsolhaaleatoriaApp();
+
+        //System.out.println("Item clicado: " + escolhaUsuario);
+    }
+
+
 
 }
