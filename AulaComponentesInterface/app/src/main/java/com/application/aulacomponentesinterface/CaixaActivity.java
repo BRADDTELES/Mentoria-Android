3ebabe6ef9b3723bdaab4072e79910c4f261cafd
BRADDTELES <braddteles@gmail.com>
2024-10-02 07:27:15 -0300

    ft.

--- AulaComponentesInterface/app/src/main/java/com/application/aulacomponentesinterface/CaixaActivity.java
+++ AulaComponentesInterface/app/src/main/java/com/application/aulacomponentesinterface/CaixaActivity.java
@@ -1,24 +1,27 @@
 package com.application.aulacomponentesinterface;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class CaixaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_caixa);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button botao = findViewById( R.id.botao_salvar );
+        TextInputEditText email = findViewById( R.id.edit_email );
+        botao.setOnClickListener(v -> {
+                String emailUsuario = email.getText().toString();
+                System.out.println("Email selecionando: " + emailUsuario);
         });
+
     }
+
 }
