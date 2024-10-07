2fe89e139aa9e652e81829ab7de8d4933e4c9fab
BRADDTELES <braddteles@gmail.com>
2024-10-07 07:02:13 -0300

    Lateinit

--- AprendendoKotlindozero/app/src/main/java/com/application/aprendendokotlindozero/MainActivity.kt
+++ AprendendoKotlindozero/app/src/main/java/com/application/aprendendokotlindozero/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.application.aprendendokotlindozero
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var texto: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,14 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //Próprio
+        texto = findViewById(R.id.textNome)
+        texto.setText("inicial")
+
+    }
+
+    fun alterarTexto(){
+        texto.setText("alterado")
     }
 }
