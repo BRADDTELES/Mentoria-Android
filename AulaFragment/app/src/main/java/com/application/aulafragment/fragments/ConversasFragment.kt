e676f50abcd7c7c05278c40d998f3718383bf531
BRADDTELES <braddteles@gmail.com>
2024-10-08 17:11:33 -0300

    Criando um Fragment

---
+++ AulaFragment/app/src/main/java/com/application/aulafragment/fragments/ConversasFragment.kt
@@ -0,0 +1,29 @@
+package com.application.aulafragment.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.application.aulafragment.R
+
+class ConversasFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(
+            R.layout.fragment_conversas,
+            container,
+            false
+        )
+
+        //Processamento da visualização
+
+        return view
+
+    }
+
+}
