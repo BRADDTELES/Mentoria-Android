c287c9ab705260997b8c7247e12152920c97d35d
BRADDTELES <braddteles@gmail.com>
2024-10-08 15:43:28 -0300

    Criando interface App Calcularadora de IMC

---
+++ AulaCalculadoraIMC/app/src/main/java/com/application/aulacalculadoraimc/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.application.aulacalculadoraimc
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnCalcular: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        btnCalcular = findViewById(R.id.btn_calcular)
+
+        btnCalcular.setOnClickListener {
+
+            val intent = Intent(this, ResultadoActivity::class.java)
+
+            startActivity( intent )
+
+        }
+
+    }
+}
