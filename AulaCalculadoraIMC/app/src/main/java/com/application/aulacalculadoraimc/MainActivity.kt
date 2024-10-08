832e8c047b0e9ba949ca76a354d3170e34492339
BRADDTELES <braddteles@gmail.com>
2024-10-08 16:13:23 -0300

    Escrevendo códigos do App Calculadora de IMC e finalizando

--- AulaCalculadoraIMC/app/src/main/java/com/application/aulacalculadoraimc/MainActivity.kt
+++ AulaCalculadoraIMC/app/src/main/java/com/application/aulacalculadoraimc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.application.aulacalculadoraimc
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,8 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
 
     private lateinit var btnCalcular: Button
+    private lateinit var editPeso: EditText
+    private lateinit var editAltura: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,10 +26,24 @@ class MainActivity : AppCompatActivity() {
         }
 
         btnCalcular = findViewById(R.id.btn_calcular)
+        editPeso = findViewById(R.id.edit_peso)
+        editAltura = findViewById(R.id.edit_altura)
 
+        /* Clique de alteração de Tela */
         btnCalcular.setOnClickListener {
-
+            /* comando para alterar a tela */
             val intent = Intent(this, ResultadoActivity::class.java)
+
+            // Recuperar valores dos campos de entrada no formatado String
+            val peso = editPeso.text.toString()
+            val altura = editAltura.text.toString()
+
+            // Validar se os campos estão preenchidos, caso esteja Vazios
+            if ( peso.isNotEmpty() && altura.isNotEmpty() ){
+                // Converter dados Strings para Double e enviar para a tela ResultadoActivity
+                intent.putExtra("peso", peso.toDouble())
+                intent.putExtra("altura", altura.toDouble())
+            }
 
             startActivity( intent )
 
