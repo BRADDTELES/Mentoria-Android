29b0180697d5a79ca6bc288aee1c34d644d8f5b4
BRADDTELES <braddteles@gmail.com>
2024-10-09 14:24:47 -0300

    Finalizado o exercicio App Gasolina ou Álcool, da forma que eu queria (minha autoria)

--- exercicioGasolinaOuAlcool/app/src/main/java/com/application/exerciciogasolinaoualcool/MainActivity.kt
+++ exercicioGasolinaOuAlcool/app/src/main/java/com/application/exerciciogasolinaoualcool/MainActivity.kt
@@ -3,11 +3,8 @@ package com.application.exerciciogasolinaoualcool
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
@@ -15,12 +12,9 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var textInputAlcool: TextInputLayout
     private lateinit var editAlcool: TextInputEditText
-
     private lateinit var textInputGasolina: TextInputLayout
     private lateinit var editGasolina: TextInputEditText
-
     private lateinit var btnCalcular: Button
-    private lateinit var textResultado: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,28 +22,35 @@ class MainActivity : AppCompatActivity() {
 
         textInputAlcool = findViewById(R.id.text_input_alcool)
         editAlcool = findViewById(R.id.edit_alcool)
-
         textInputGasolina = findViewById(R.id.text_input_gasolina)
         editGasolina = findViewById(R.id.edit_gasolina)
 
         btnCalcular = findViewById(R.id.btn_calcular)
 
         btnCalcular.setOnClickListener {
-            val intent = Intent(this, DetalhesActivity::class.java)
 
             val alcool = editAlcool.text.toString()
             val gasolina = editGasolina.text.toString()
 
             if ( alcool.isNotEmpty() && gasolina.isNotEmpty() ){
+                // Mudando de tela com o clique, após dados válidados
+                val intent = Intent(this, DetalhesActivity::class.java)
                 intent.putExtra("alcool", alcool.toDouble())
                 intent.putExtra("gasolina", gasolina.toDouble())
+                startActivity(intent)
+            }else{
+                textInputAlcool.error = null
+                textInputGasolina.error = null
+                if ( alcool.isEmpty() ){
+                    textInputAlcool.error = "Opção inválida, digite o preço do álcool"
+                    Toast.makeText(this, "Preencha o campo", Toast.LENGTH_SHORT).show()
+                }
+                if ( gasolina.isEmpty() ){
+                    textInputGasolina.error = "Opção inválida, digite o preço da gasolina"
+                    Toast.makeText(this, "Preencha o campo", Toast.LENGTH_SHORT).show()
+                }
             }
 
-            startActivity( intent )
-
         }
-
     }
-
-
 }
