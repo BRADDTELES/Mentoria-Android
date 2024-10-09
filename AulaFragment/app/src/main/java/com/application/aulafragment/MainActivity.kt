c123319535d787f1452ea1e5c46eedf1c5e5c431
BRADDTELES <braddteles@gmail.com>
2024-10-09 06:31:12 -0300

    Ciclo de vida de Fragments

--- AulaFragment/app/src/main/java/com/application/aulafragment/MainActivity.kt
+++ AulaFragment/app/src/main/java/com/application/aulafragment/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.application.aulafragment
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -16,13 +17,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        Log.i("ciclo_vida", "Activity onCreate")
 
         btnConversas = findViewById(R.id.btn_conversas)
         btnChamadas = findViewById(R.id.btn_chamadas)
@@ -50,5 +47,30 @@ class MainActivity : AppCompatActivity() {
                 .commit()
         }
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("ciclo_vida", "Activity onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("ciclo_vida", "Activity onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("ciclo_vida", "Activity onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("ciclo_vida", "Activity onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("ciclo_vida", "Activity onDestroy")
     }
 }
--- AulaFragment/app/src/main/java/com/application/aulafragment/fragments/ConversasFragment.kt
+++ AulaFragment/app/src/main/java/com/application/aulafragment/fragments/ConversasFragment.kt
@@ -1,6 +1,8 @@
 package com.application.aulafragment.fragments
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,11 +11,22 @@ import com.application.aulafragment.R
 
 class ConversasFragment : Fragment() {
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.i("ciclo_vida", "Fragment onAttach")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.i("ciclo_vida", "Fragment onCreate")
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Log.i("ciclo_vida", "Fragment onCreateView")
         val view = inflater.inflate(
             R.layout.fragment_conversas,
             container,
@@ -26,4 +39,48 @@ class ConversasFragment : Fragment() {
 
     }
 
+    /* Depreciado */
+    /*override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }*/
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.i("ciclo_vida", "Fragment onViewCreated")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("ciclo_vida", "Fragment onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("ciclo_vida", "Fragment onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("ciclo_vida", "Fragment onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("ciclo_vida", "Fragment onStop")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.i("ciclo_vida", "Fragment onDestroyView")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("ciclo_vida", "Fragment onDestroy")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.i("ciclo_vida", "Fragment onDetach")
+    }
 }
