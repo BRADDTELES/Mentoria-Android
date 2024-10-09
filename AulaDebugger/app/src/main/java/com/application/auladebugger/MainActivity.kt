9027b6c610dafcb9abf8e5e90b3b5a482fffc672
BRADDTELES <braddteles@gmail.com>
2024-10-09 14:54:09 -0300

    Introdução ao Debugger no Android Studio

---
+++ AulaDebugger/app/src/main/java/com/application/auladebugger/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.application.auladebugger
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val listaUsuarios = listOf("Danillo", "Ana", "Maria", "João")
+        exibirListaItens ( listaUsuarios )
+    }
+
+    private fun exibirListaItens(listaUsuarios: List<String>) {
+
+        var exibirPrimeiroUsuario = true
+        var contadorItens = 0 // Break Point (ponto de parada)
+
+        for ( usuario in listaUsuarios ){
+            if ( exibirPrimeiroUsuario ){
+                println("primeiro usuário: ")
+                exibirPrimeiroUsuario = false
+            }
+            println(usuario)
+            contadorItens++
+        }
+    }
+}
