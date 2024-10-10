9b52b1a5e83c5c060820557aa8f2fc5b6e904809
BRADDTELES <braddteles@gmail.com>
2024-10-10 13:56:06 -0300

    Desestruturação (Destructuring)

--- AulaComponentesListagemColecoes/app/src/main/java/com/application/aulacomponenteslistagemcolecoes/teste/classes.kt
+++ AulaComponentesListagemColecoes/app/src/main/java/com/application/aulacomponenteslistagemcolecoes/teste/classes.kt
@@ -13,9 +13,14 @@ data class Pergunta( val pergunta: String, val respostaCerta: Int )
 fun main() {
 
     val pergunta1 = Pergunta( "Qual a pergunta?", 1 )
-    val pergunta2 = Pergunta( "Qual a pergunta?", 1 )
+    val pergunta2 = Pergunta( "Qual a pergunta?", 2 )
 
-    println( pergunta1 == pergunta2 )
+    val (pergunta, resposta) = pergunta1
+
+    println(pergunta)
+    println(resposta)
+
+    //println( pergunta1 == pergunta2 )
 
     /*val motorista = Motorista("Danillo")
     val caminhao = motorista.Caminhao("FH 60")
