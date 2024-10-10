3b40097e7907ec5d1ff1ceb315307588222be2ae
BRADDTELES <braddteles@gmail.com>
2024-10-10 09:57:53 -0300

    Eventos de clique & R.java

---
+++ AulaComponentesListagemColecoes/app/src/main/java/com/application/aulacomponenteslistagemcolecoes/teste/Recursos.java
@@ -0,0 +1,9 @@
+package com.application.aulacomponenteslistagemcolecoes.teste;
+
+public class Recursos {
+
+    public static final class id {
+        public static final int text_numero = 1009;
+    }
+
+}
