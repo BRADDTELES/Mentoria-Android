30230716170cc706c6193000d76d476e260b544d
BRADDTELES <braddteles@gmail.com>
2024-10-10 13:52:12 -0300

    Classes de dados (Data Class)

--- AulaComponentesListagemColecoes/app/src/main/java/com/application/aulacomponenteslistagemcolecoes/teste/classes.kt
+++ AulaComponentesListagemColecoes/app/src/main/java/com/application/aulacomponenteslistagemcolecoes/teste/classes.kt
@@ -1,18 +1,25 @@
 package com.application.aulacomponenteslistagemcolecoes.teste
 
-class Motorista ( val nome: String ){
+/*class Motorista ( val nome: String ){
     fun exibirDadosMotorista() = println("Motorista: $nome")
 
     inner class Caminhao( val nomeCaminhao: String ){//Classe aninhada (Nested Class)
         fun exibirDadosCaminhao() = println("Caminhão: $nomeCaminhao motorista: $nome")
     }
-}
+}*/
+
+data class Pergunta( val pergunta: String, val respostaCerta: Int )
 
 fun main() {
 
-    val motorista = Motorista("Danillo")
+    val pergunta1 = Pergunta( "Qual a pergunta?", 1 )
+    val pergunta2 = Pergunta( "Qual a pergunta?", 1 )
+
+    println( pergunta1 == pergunta2 )
+
+    /*val motorista = Motorista("Danillo")
     val caminhao = motorista.Caminhao("FH 60")
-    caminhao.exibirDadosCaminhao()
+    caminhao.exibirDadosCaminhao()*/
 
     /*val caminhao = Motorista.Caminhao("FH 500")
     caminhao.exibirDadosCaminhao()*/
