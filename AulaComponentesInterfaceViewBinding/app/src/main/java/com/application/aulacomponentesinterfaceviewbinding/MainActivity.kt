f9ead2162ae562b21bd187056b4c52e0fb84e751
BRADDTELES <braddteles@gmail.com>
2024-10-15 14:46:24 -0300

    Grupos de visualizações (ViewGroup)

--- AulaComponentesInterfaceViewBinding/app/src/main/java/com/application/aulacomponentesinterfaceviewbinding/MainActivity.kt
+++ AulaComponentesInterfaceViewBinding/app/src/main/java/com/application/aulacomponentesinterfaceviewbinding/MainActivity.kt
@@ -2,11 +2,14 @@ package com.application.aulacomponentesinterfaceviewbinding
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 import com.application.aulacomponentesinterfaceviewbinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +24,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //binding = ActivityMainBinding.inflate( layoutInflater )
         setContentView( binding.root )
+
+        //Button -> Button, TextView, View
+        //TextView -> TextView, View
+        //LinearLayout -> LinearLayout, ViewGroup, View
+        //RecyclerView -> RecyclerView, ViewGroup, View
 
         with(binding){
             btnClique?.setOnClickListener {  }
