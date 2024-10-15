8721fa72c6b17657f58cbcc99bfb25491acee80d
BRADDTELES <braddteles@gmail.com>
2024-10-15 17:07:10 -0300

    Mais sobre Floating Action Button

--- AulaComponentesInterfaceViewBinding/app/src/main/java/com/application/aulacomponentesinterfaceviewbinding/FloatingActionButtonActivity.kt
+++ AulaComponentesInterfaceViewBinding/app/src/main/java/com/application/aulacomponentesinterfaceviewbinding/FloatingActionButtonActivity.kt
@@ -1,14 +1,30 @@
 package com.application.aulacomponentesinterfaceviewbinding
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import com.application.aulacomponentesinterfaceviewbinding.databinding.ActivityFloatingActionButtonBinding
 
 class FloatingActionButtonActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityFloatingActionButtonBinding.inflate( layoutInflater )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_floating_action_button)
+        setContentView( binding.root )
+
+        with( binding ){
+            fabBotao.setOnClickListener {
+
+                groupMenu.visibility = if ( groupMenu.isVisible ){
+                    View.INVISIBLE
+                }else{
+                    View.VISIBLE
+                }
+            }
+        }
     }
 }
