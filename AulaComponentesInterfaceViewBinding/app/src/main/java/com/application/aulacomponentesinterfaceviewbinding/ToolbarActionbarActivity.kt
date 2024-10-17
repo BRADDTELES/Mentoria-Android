d6c98709425c915fd70308f1c2c49ee74c395652
BRADDTELES <braddteles@gmail.com>
2024-10-17 09:38:46 -0300

    Customizando Toolbar (minha própria Barra Superior Principal)

--- AulaComponentesInterfaceViewBinding/app/src/main/java/com/application/aulacomponentesinterfaceviewbinding/ToolbarActionbarActivity.kt
+++ AulaComponentesInterfaceViewBinding/app/src/main/java/com/application/aulacomponentesinterfaceviewbinding/ToolbarActionbarActivity.kt
@@ -41,7 +41,34 @@ class ToolbarActionbarActivity : AppCompatActivity() {
             ContextCompat.getColor(this, R.color.white)
         )*/
 
+        /*binding.tbPrincipal.inflateMenu(R.menu.menu_principal)
+        binding.tbPrincipal.setOnMenuItemClickListener { menuItem ->
+            when( menuItem.itemId ){
+                R.id.item_adicionar -> {
+                    Toast.makeText(applicationContext,"Adicionar", Toast.LENGTH_SHORT).show()
+                    return@setOnMenuItemClickListener true
+                }
+                R.id.item_pesquisar -> {
+                    Toast.makeText(applicationContext, "Pesquisar", Toast.LENGTH_SHORT).show()
+                    return@setOnMenuItemClickListener true
+                }
+                R.id.item_configuracoes -> {
+                    Toast.makeText(applicationContext, "Configurações", Toast.LENGTH_SHORT).show()
+                    return@setOnMenuItemClickListener true
+                }
+                R.id.item_sair -> {
+                    Toast.makeText(applicationContext, "Sair", Toast.LENGTH_SHORT).show()
+                    return@setOnMenuItemClickListener true
+                }
+
+                else -> {
+                    return@setOnMenuItemClickListener true
+                }
+            }
+        }*/
+
         setSupportActionBar( binding.tbPrincipal )
+        inicializarActionBar()
     }
 
     private fun inicializarActionBar() {
