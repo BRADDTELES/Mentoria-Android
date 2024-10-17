55a0ce75e9ce4366e842f1358830c408d71e4caa
BRADDTELES <braddteles@gmail.com>
2024-10-17 07:05:50 -0300

    Toolbar vs Actionbar

---
+++ AulaComponentesInterfaceViewBinding/app/src/main/java/com/application/aulacomponentesinterfaceviewbinding/ToolbarActionbarActivity.kt
@@ -0,0 +1,20 @@
+package com.application.aulacomponentesinterfaceviewbinding
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ToolbarActionbarActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_toolbar_actionbar)
+
+        //supportActionBar?.hide()
+
+    }
+}
