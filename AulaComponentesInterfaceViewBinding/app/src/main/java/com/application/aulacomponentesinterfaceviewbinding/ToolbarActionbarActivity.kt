e43b85a4d4b937b0326f15cb4f351a7110941891
BRADDTELES <braddteles@gmail.com>
2024-10-17 07:25:54 -0300

    Criando Menus para Toolbar, aula importantissima

--- AulaComponentesInterfaceViewBinding/app/src/main/java/com/application/aulacomponentesinterfaceviewbinding/ToolbarActionbarActivity.kt
+++ AulaComponentesInterfaceViewBinding/app/src/main/java/com/application/aulacomponentesinterfaceviewbinding/ToolbarActionbarActivity.kt
@@ -1,6 +1,9 @@
 package com.application.aulacomponentesinterfaceviewbinding
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,29 @@ class ToolbarActionbarActivity : AppCompatActivity() {
 
         //supportActionBar?.hide()
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate( R.menu.menu_principal, menu )
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when( item.itemId ){
+            R.id.item_adicionar -> {
+                Toast.makeText(this, "Adicionar", Toast.LENGTH_SHORT).show()
+            }
+            R.id.item_pesquisar -> {
+                Toast.makeText(this, "Pesquisar", Toast.LENGTH_SHORT).show()
+            }
+            R.id.item_configuracoes -> {
+                Toast.makeText(this, "Configurações", Toast.LENGTH_SHORT).show()
+            }
+            R.id.item_sair -> {
+                Toast.makeText(this, "Sair", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return true
     }
 }
