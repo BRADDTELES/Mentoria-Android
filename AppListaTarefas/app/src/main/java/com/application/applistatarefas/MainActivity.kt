58eb3b6d8e9993620c7140217046beea1245c351
BRADDTELES <braddteles@gmail.com>
2024-10-20 20:08:52 -0300

    App Lista de Tarefas - Exibindo tarefas no RecyclerView (atualizando a lista de tarefas e mostrando a lista)

--- AppListaTarefas/app/src/main/java/com/application/applistatarefas/MainActivity.kt
+++ AppListaTarefas/app/src/main/java/com/application/applistatarefas/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.application.applistatarefas.adapter.TarefaAdapter
 import com.application.applistatarefas.database.TarefaDAO
 import com.application.applistatarefas.databinding.ActivityMainBinding
 import com.application.applistatarefas.model.Tarefa
@@ -18,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private var listaTarefas = emptyList<Tarefa>() // Lista de tarefas vazia
+    private var tarefaAdapter: TarefaAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,17 +31,24 @@ class MainActivity : AppCompatActivity() {
             startActivity( intent )
         }
 
+        //RecyclerView
+        tarefaAdapter = TarefaAdapter()
+        binding.rvTarefas.adapter = tarefaAdapter
 
+        binding.rvTarefas.layoutManager = LinearLayoutManager(this)
+
+    }
+
+    private fun atualizarListaTarefas(){
+
+        val tarefaDAO = TarefaDAO(this)
+        listaTarefas = tarefaDAO.listar()
+        tarefaAdapter?.adicionarLista( listaTarefas )
 
     }
 
     override fun onStart() {
         super.onStart()
-        val tarefaDAO = TarefaDAO(this)
-        listaTarefas = tarefaDAO.listar()
-
-        listaTarefas.forEach { tarefa ->
-            Log.i("info_db", " ${tarefa.descricao} \n")
-        }
+        atualizarListaTarefas()
     }
 }
