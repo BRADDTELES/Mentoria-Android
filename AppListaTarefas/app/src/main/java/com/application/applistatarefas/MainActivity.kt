fdf8cf68dfc359c3c5cfff8d9b1a9008d27cc037
BRADDTELES <braddteles@gmail.com>
2024-10-20 19:45:17 -0300

    App Lista de Tarefas - Listando tarefas (prestar atenção na criação do onStart())

--- AppListaTarefas/app/src/main/java/com/application/applistatarefas/MainActivity.kt
+++ AppListaTarefas/app/src/main/java/com/application/applistatarefas/MainActivity.kt
@@ -2,17 +2,22 @@ package com.application.applistatarefas
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.application.applistatarefas.database.TarefaDAO
 import com.application.applistatarefas.databinding.ActivityMainBinding
+import com.application.applistatarefas.model.Tarefa
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private var listaTarefas = emptyList<Tarefa>() // Lista de tarefas vazia
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +26,19 @@ class MainActivity : AppCompatActivity() {
         binding.fabAdicionar.setOnClickListener {
             val intent = Intent(this, AdicionarTarefaActivity::class.java)
             startActivity( intent )
+        }
+
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val tarefaDAO = TarefaDAO(this)
+        listaTarefas = tarefaDAO.listar()
+
+        listaTarefas.forEach { tarefa ->
+            Log.i("info_db", " ${tarefa.descricao} \n")
         }
     }
 }
--- AppListaTarefas/app/src/main/java/com/application/applistatarefas/database/TarefaDAO.kt
+++ AppListaTarefas/app/src/main/java/com/application/applistatarefas/database/TarefaDAO.kt
@@ -41,6 +41,32 @@ class TarefaDAO(context: Context) : ITarefaDAO {
     }
 
     override fun listar(): List<Tarefa> {
-        TODO("Not yet implemented")
+
+        val listaTarefas = mutableListOf<Tarefa>()
+
+        val sql = "SELECT ${DatabaseHelper.COLUNA_ID_TAREFA}, " +
+                "${DatabaseHelper.COLUNA_DESCRICAO}, " +
+                "strftime('%d/%m/%Y %H:%M', ${DatabaseHelper.COLUNA_DATA_CADASTRO}) ${DatabaseHelper.COLUNA_DATA_CADASTRO} " +
+                "FROM ${DatabaseHelper.NOME_TABELA_TAREFAS};"
+
+        val cursor = leitura.rawQuery( sql, null )
+
+        val indiceId = cursor.getColumnIndex( DatabaseHelper.COLUNA_ID_TAREFA )
+        val indiceDescricao = cursor.getColumnIndex( DatabaseHelper.COLUNA_DESCRICAO )
+        val indiceDataCadastro = cursor.getColumnIndex( DatabaseHelper.COLUNA_DATA_CADASTRO )
+
+        while ( cursor.moveToNext() ){
+
+            val idTarefa = cursor.getInt(indiceId)
+            val descricao = cursor.getString(indiceDescricao)
+            val dataCadastro = cursor.getString(indiceDataCadastro)
+
+            listaTarefas.add(
+                Tarefa(idTarefa, descricao, dataCadastro)
+            )
+
+        }
+
+        return listaTarefas
     }
 }
