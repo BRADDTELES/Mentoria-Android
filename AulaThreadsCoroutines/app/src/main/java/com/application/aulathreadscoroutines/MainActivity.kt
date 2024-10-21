bee21641554e05c8bc4e0bbfdcbb6d07d488d569
BRADDTELES <braddteles@gmail.com>
2024-10-21 06:14:27 -0300

    Configurando projeto e Interface (sobre Threads)

---
+++ AulaThreadsCoroutines/app/src/main/java/com/application/aulathreadscoroutines/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.application.aulathreadscoroutines
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.application.aulathreadscoroutines.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.btnAbrir.setOnClickListener {
+            startActivity(
+                Intent(this, SegundaActivity::class.java)
+            )
+        }
+
+        binding.btnIniciar.setOnClickListener {
+
+        }
+
+
+    }
+}
---
+++ AulaThreadsCoroutines/app/src/main/java/com/application/aulathreadscoroutines/SegundaActivity.kt
@@ -0,0 +1,20 @@
+package com.application.aulathreadscoroutines
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SegundaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_segunda)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
