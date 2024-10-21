04081cba543931e8f2b5b0ee395083ef6d4ff978
BRADDTELES <braddteles@gmail.com>
2024-10-20 21:07:04 -0300

    Configurando o projeto & Criando interface

---
+++ AulaRealm/app/src/main/java/com/application/aularealm/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.application.aularealm
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.application.aularealm.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.btnSalvar.setOnClickListener {
+
+            val nome = binding.editNome.text.toString()
+
+        }
+
+    }
+}
