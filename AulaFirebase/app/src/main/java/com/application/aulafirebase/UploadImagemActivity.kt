2a1b7a76bacca30ea08423926e486fe64555cb59
BRADDTELES <braddteles@gmail.com>
2024-11-06 16:12:33 -0300

    Abrindo galeria de imagens

--- AulaFirebase/app/src/main/java/com/application/aulafirebase/UploadImagemActivity.kt
+++ AulaFirebase/app/src/main/java/com/application/aulafirebase/UploadImagemActivity.kt
@@ -1,6 +1,8 @@
 package com.application.aulafirebase
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.application.aulafirebase.databinding.ActivityUploadImagemBinding
 
@@ -9,12 +11,24 @@ class UploadImagemActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityUploadImagemBinding.inflate( layoutInflater )
     }
+    private val abrirGaleria = registerForActivityResult(
+        ActivityResultContracts.GetContent()
+    ){ uri ->
+        if (uri != null){
+            binding.imageSelecionada.setImageURI( uri )
+            Toast.makeText(this, "imagem selecionada", Toast.LENGTH_SHORT).show()
+        }else{
+            Toast.makeText(this, "Nenhuma imagem selecionada", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView( binding.root )
 
-
+        binding.btnGaleria.setOnClickListener {
+            abrirGaleria.launch("image/*")//Mime Type
+        }
 
     }
 }
