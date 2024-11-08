451e0b0d0f6b155a9f0a61ae3146469e80001082
BRADDTELES <braddteles@gmail.com>
2024-11-08 17:30:38 -0300

    Fazendo a funcionalidade de Deslogar na codificação

--- ProjetoTurmaFirebase/app/src/main/java/com/application/projetoturmafirebase/MainActivity.kt
+++ ProjetoTurmaFirebase/app/src/main/java/com/application/projetoturmafirebase/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.application.projetoturmafirebase
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.application.projetoturmafirebase.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    private val autenticacao by lazy {
+        FirebaseAuth.getInstance()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView( binding.root )
+
+        with(binding){
+            btnDeslogar.setOnClickListener {
+                autenticacao.signOut()
+                startActivity(
+                    Intent(applicationContext, LoginActivity::class.java) //Intent não funciona em uma função de escopo, para isso precisa trocar o this = applicationContext
+                )
+            }
+        }
+
     }
 }
