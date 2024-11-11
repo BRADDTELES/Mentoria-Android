e6695b26d23e620eedac9ef57a0bbc860828fa59
BRADDTELES <braddteles@gmail.com>
2024-11-11 10:59:54 -0300

    Validação e cadastro de usuario

--- AulaWhatsApp/app/src/main/java/com/application/aulawhatsapp/CadastroActivity.kt
+++ AulaWhatsApp/app/src/main/java/com/application/aulawhatsapp/CadastroActivity.kt
@@ -1,8 +1,15 @@
 package com.application.aulawhatsapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.application.aulawhatsapp.databinding.ActivityCadastroBinding
+import com.application.aulawhatsapp.utils.exibirMensagem
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 
 class CadastroActivity : AppCompatActivity() {
 
@@ -10,11 +17,81 @@ class CadastroActivity : AppCompatActivity() {
         ActivityCadastroBinding.inflate(layoutInflater)
     }
 
+    private lateinit var nome: String
+    private lateinit var email: String
+    private lateinit var senha: String
+    private val firebaseAuth by lazy {
+        FirebaseAuth.getInstance()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
         inicializarToolbar()
+        inicializarEventosClique()
+    }
+
+    private fun inicializarEventosClique() {
+
+        binding.btnCadastrar.setOnClickListener {
+            if ( validarCampos()){
+                cadastrarUsuario(nome, email, senha)
+            }
+        }
+
+    }
+
+    private fun cadastrarUsuario(nome: String, email: String, senha: String) {
+        firebaseAuth.createUserWithEmailAndPassword(
+            email, senha
+        ).addOnCompleteListener { resultado ->
+            if (resultado.isSuccessful){
+                exibirMensagem("Sucesso ao fazer seu cadastro")
+                startActivity(
+                    Intent(applicationContext, MainActivity::class.java)
+                )
+            }
+        }.addOnFailureListener { erro ->
+            try {
+                throw erro
+            }catch (erroCredenciaisInvalidas: FirebaseAuthInvalidCredentialsException){
+                erroCredenciaisInvalidas.printStackTrace()
+                exibirMensagem("E-mail inválido, digite um outro e-mail")
+            }catch (erroUsuarioExistente: FirebaseAuthUserCollisionException){
+                erroUsuarioExistente.printStackTrace()
+                exibirMensagem("E-mail já pertence a outro e-mail")
+            }catch (erroSenhaFraca: FirebaseAuthWeakPasswordException){
+                erroSenhaFraca.printStackTrace()
+                exibirMensagem("Senha fraca, digite outra com letras, número e caracteres especiais")
+            }
+        }
+    }
+
+    private fun validarCampos(): Boolean {
+
+        nome = binding.editNome.text.toString()
+        email = binding.editEmail.text.toString()
+        senha = binding.editSenha.text.toString()
+
+        if (nome.isNotEmpty()){
+            binding.textInputNome.error = null
+            if (email.isNotEmpty()){
+                binding.textInputEmail.error = null
+                if (senha.isNotEmpty()){
+                    binding.textInputSenha.error = null
+                    return true
+                }else{
+                    binding.textInputSenha.error = "Preencha a senha!"
+                    return false
+                }
+            }else{
+                binding.textInputEmail.error = "Preencha o seu e-mail!"
+                return false
+            }
+        }else{
+            binding.textInputNome.error = "Preencha o seu nome!"
+            return false
+        }
     }
 
     private fun inicializarToolbar() {
