f35dfe20a5124b95b14af701fa00f072b60089a3
BRADDTELES <braddteles@gmail.com>
2024-11-12 06:48:57 -0300

    Criando interface de perfil

--- AulaWhatsApp/app/src/main/java/com/application/aulawhatsapp/PerfilActivity.kt
+++ AulaWhatsApp/app/src/main/java/com/application/aulawhatsapp/PerfilActivity.kt
@@ -3,12 +3,13 @@ package com.application.aulawhatsapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.application.aulawhatsapp.databinding.ActivityMainBinding
+import com.application.aulawhatsapp.databinding.ActivityPerfilBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class PerfilActivity : AppCompatActivity() {
 
     private val binding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
+        ActivityPerfilBinding.inflate(layoutInflater)
     }
     private val firebaseAuth by lazy {
         FirebaseAuth.getInstance()
@@ -16,6 +17,16 @@ class PerfilActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        inicializarToolbar()
 
+    }
+
+    private fun inicializarToolbar() {
+        val toolbar = binding.includeToolbarPerfil.tbPrincipal
+        setSupportActionBar( toolbar )
+        supportActionBar?.apply {
+            title = "Editar perfil"
+            setDisplayHomeAsUpEnabled(true)
+        }
     }
 }
