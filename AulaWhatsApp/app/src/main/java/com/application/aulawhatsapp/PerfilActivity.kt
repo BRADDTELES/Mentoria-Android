dd4e9f27e09f110c6f4288d37f5bee2ec9c88c6d
BRADDTELES <braddteles@gmail.com>
2024-11-12 07:15:21 -0300

    Solicitando permissões

--- AulaWhatsApp/app/src/main/java/com/application/aulawhatsapp/PerfilActivity.kt
+++ AulaWhatsApp/app/src/main/java/com/application/aulawhatsapp/PerfilActivity.kt
@@ -1,7 +1,12 @@
 package com.application.aulawhatsapp
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.application.aulawhatsapp.databinding.ActivityMainBinding
 import com.application.aulawhatsapp.databinding.ActivityPerfilBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -11,13 +16,53 @@ class PerfilActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityPerfilBinding.inflate(layoutInflater)
     }
-    private val firebaseAuth by lazy {
-        FirebaseAuth.getInstance()
-    }
+    private var temPermissaoCamera = false
+    private var temPermissaoGaleria = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         inicializarToolbar()
+        solicitarPermissoes()
+
+    }
+
+    private fun solicitarPermissoes() {
+
+        //Verificar permissões
+        temPermissaoCamera = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+
+        temPermissaoGaleria = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.READ_MEDIA_IMAGES
+        ) == PackageManager.PERMISSION_GRANTED
+
+        //LISTA DE PERMISSÇOES NEGADAS
+        val listaPermissoesNegadas = mutableListOf<String>()
+        if ( !temPermissaoCamera )
+            listaPermissoesNegadas.add( Manifest.permission.CAMERA )
+
+        if ( !temPermissaoGaleria )
+            listaPermissoesNegadas.add( Manifest.permission.READ_MEDIA_IMAGES )
+
+        if ( listaPermissoesNegadas.isNotEmpty() ){
+            //Solicitar multiplas permissões
+            val gerenciadorPermissoes = registerForActivityResult(
+                ActivityResultContracts.RequestMultiplePermissions()
+            ){ permissoes ->
+
+                temPermissaoCamera = permissoes[Manifest.permission.CAMERA]
+                    ?: temPermissaoCamera
+
+                temPermissaoGaleria = permissoes[Manifest.permission.READ_MEDIA_IMAGES]
+                    ?: temPermissaoGaleria
+
+            }
+            gerenciadorPermissoes.launch( listaPermissoesNegadas.toTypedArray() )
+        }
 
     }
 
