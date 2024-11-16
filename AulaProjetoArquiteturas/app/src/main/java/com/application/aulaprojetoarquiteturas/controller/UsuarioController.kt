0f90b6a9a1f7010de8d09e3d7bdb1a3d06fe4a3c
BRADDTELES <braddteles@gmail.com>
2024-11-15 21:31:21 -0300

    Simulando recuperação de dados da API

--- AulaProjetoArquiteturas/app/src/main/java/com/application/aulaprojetoarquiteturas/controller/UsuarioController.kt
+++ AulaProjetoArquiteturas/app/src/main/java/com/application/aulaprojetoarquiteturas/controller/UsuarioController.kt
@@ -1,4 +1,25 @@
 package com.application.aulaprojetoarquiteturas.controller
 
-class UsuarioController {
+import com.application.aulaprojetoarquiteturas.model.Usuario
+import com.application.aulaprojetoarquiteturas.model.api.UsuarioAPI
+import com.application.aulaprojetoarquiteturas.view.MainActivity
+
+class UsuarioController(
+    private val mainActivity: MainActivity
+) {
+
+    private val usuarioAPI = UsuarioAPI()
+
+    init {
+        recuperarUsuario()
+    }
+
+    fun recuperarUsuario() {
+
+        //1) Regra de négocio
+        val lista = usuarioAPI.recuperarUsuario()
+
+        //2) Exibir os dados na interface
+        mainActivity.listarUsuarios( lista )
+    }
 }
--- AulaProjetoArquiteturas/app/src/main/java/com/application/aulaprojetoarquiteturas/view/MainActivity.kt
+++ AulaProjetoArquiteturas/app/src/main/java/com/application/aulaprojetoarquiteturas/view/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.application.aulaprojetoarquiteturas.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.application.aulaprojetoarquiteturas.R
+import com.application.aulaprojetoarquiteturas.controller.UsuarioController
+import com.application.aulaprojetoarquiteturas.model.Usuario
 
 class MainActivity : AppCompatActivity() {
+
+    // as 2 linhas abaixo podem ser utilizadas de uma ou de outra
+    //private var usuarioController: UsuarioController? = null
+    private lateinit var usuarioController: UsuarioController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        usuarioController = UsuarioController(
+            this
+        )
+        //usuarioController.recuperarUsuario()//Ativa
+
+    }
+
+    //Passiva
+    fun listarUsuarios( lista: List<Usuario> ){
+        //Exibir os dados
+        println(lista)
+        Log.i("teste", "${lista.toString()}")
     }
 }
