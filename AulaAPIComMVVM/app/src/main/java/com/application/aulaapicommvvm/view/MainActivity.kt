6d941b490dad9b3614370f0e94f9e20199c3d067
BRADDTELES <braddteles@gmail.com>
2024-11-17 22:16:43 -0300

    API com MVVM - Exibindo postagens

--- AulaAPIComMVVM/app/src/main/java/com/application/aulaapicommvvm/view/MainActivity.kt
+++ AulaAPIComMVVM/app/src/main/java/com/application/aulaapicommvvm/view/MainActivity.kt
@@ -2,18 +2,36 @@ package com.application.aulaapicommvvm.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.application.aulaapicommvvm.databinding.ActivityMainBinding
+import com.application.aulaapicommvvm.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy {
         ActivityMainBinding.inflate( layoutInflater )
     }
+    private lateinit var mainViewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView( binding.root )
 
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class]
 
+        mainViewModel.listaPostagens.observe(this){ listaPostagens ->
 
+            var listaResultado = ""
+            listaPostagens.forEach { postagem ->
+                listaResultado += "${postagem.id}) ${postagem.title} \n"
+            }
+            binding.textResultado.text = listaResultado
+
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mainViewModel.recuperarPostagens()
     }
 }
