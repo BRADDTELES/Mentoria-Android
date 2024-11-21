7812fa585ff127f08f2b979d64820283984780dc
BRADDTELES <braddteles@gmail.com>
2024-11-20 23:31:24 -0300

    Sealed class com Arquitetura

--- AulaSealedEnumArquitetura/app/src/main/java/com/danilloteles/aulasealedenumarquitetura/SealedVsEnum.kt
+++ AulaSealedEnumArquitetura/app/src/main/java/com/danilloteles/aulasealedenumarquitetura/SealedVsEnum.kt
@@ -16,11 +16,6 @@ package com.danilloteles.aulasealedenumarquitetura
 /*enum class StatusRequisicaoEnum {//Armazena constantes
     Sucesso, ERRO
 }*/
-sealed class StatusRequisicao {
-    //Armazena objetos
-    class Sucesso(var lista: List<String>) : StatusRequisicao()
-    class Erro(var codigo: Int, var descricao: String) : StatusRequisicao()
-}
 
 /*sealed interface StatusRequisicao {
     //Armazena objetos
@@ -28,19 +23,57 @@ sealed class StatusRequisicao {
     class Erro(var codigo: Int, var descricao: String) : StatusRequisicao
 }*/
 
+sealed class StatusRequisicao {
+    //Armazena objetos
+    class Sucesso(var lista: List<String>) : StatusRequisicao()
+    class Erro(var codigo: Int, var descricao: String) : StatusRequisicao()
+}
+
+data class Usuario(val nome: String)
+
+class UsuarioRepository{
+    fun salvarUsuario(usuario: Usuario, retornoRequisicao: (StatusRequisicao) -> Unit ) {
+        val lista = listOf("jamilton", "ana", "maria")
+        //val status = StatusRequisicao.Sucesso( lista )
+        val status = StatusRequisicao.Erro(10, "Erro comunicação API")
+        retornoRequisicao.invoke( status )
+    }
+}
+
+class UsuarioViewModel{
+    fun salvarUsuario(usuario: Usuario, retornoRequisicao: (StatusRequisicao) -> Unit ){
+        val usuarioRepository = UsuarioRepository()
+        val listaUsuarios = usuarioRepository.salvarUsuario( usuario, retornoRequisicao )
+    }
+}
+
 fun main() {
+
+    //Activity
+    val usuario = Usuario("Jamilton")
+    val usuarioViewModel = UsuarioViewModel()
+    usuarioViewModel.salvarUsuario( usuario ){ statusRequisicao ->
+        when ( statusRequisicao ){
+            is StatusRequisicao.Sucesso -> println("Sucesso lista: ${statusRequisicao.lista}")
+            is StatusRequisicao.Erro -> println("Erro: ${statusRequisicao.codigo} desc: ${statusRequisicao.descricao}")
+        }
+    }
+
+
+
+
 
     /*val status = StatusPedido.AGUARDANDO
     if ( status == StatusPedido.AGUARDANDO ){}*/
 
     //Sealed Class
-    val statusRequisicao: StatusRequisicao = StatusRequisicao.Sucesso(
+    /*val statusRequisicao: StatusRequisicao = StatusRequisicao.Sucesso(
         listOf("jamilton", "ana", "maria")
     )
     when ( statusRequisicao ){
         is StatusRequisicao.Sucesso -> println("Sucesso lista: ${statusRequisicao.lista}")
         is StatusRequisicao.Erro -> println("Erro: ${statusRequisicao.codigo} desc: ${statusRequisicao.descricao}")
-    }
+    }*/
 
     //Enum
     /*val statusRequisicao = StatusRequisicaoEnum.Sucesso
