2924fb9ff5fed677195e0b070b6142a86994406e
BRADDTELES <braddteles@gmail.com>
2024-12-03 06:42:22 -0300

    Utilizando Assert com a Truth

--- AulaTestes/app/src/test/java/com/application/aulatestes/TruthTest.kt
+++ AulaTestes/app/src/test/java/com/application/aulatestes/TruthTest.kt
@@ -23,7 +23,44 @@ class TruthTest {
       ).contains("jamilton")
       assertThat(// Afirmar que
          "jamilton"
-      ).contains("a")*/
+      ).contains("a")
+      val meuItem = listOf("marcelo")
+      assertThat(// Afirmar que
+         listOf("jamilton", "ana")
+      ).containsAnyIn( meuItem )
+      assertThat(// Afirmar que
+         listOf("jamilton", "ana")
+      ).hasSize(2)
+      assertThat(// Afirmar que
+         "ana"
+      ).hasLength(3)
+      assertThat(// Afirmar que
+         //"jamilton"
+         listOf("jamilton", "ana")
+      ).isNotEmpty()
+      assertThat(// Afirmar que
+         true
+      ).isEqualTo(false)
+      val numero = 5
+      assertThat(// Afirmar que
+         numero
+      ).isIn(8..20)
+      val numero = 5
+      assertThat(// Afirmar que
+         numero
+      //).isGreaterThan(10)// Maior do que
+      ).isLessThan(10)// Menor do que
+      val numero: Int? = null
+      assertThat(// Afirmar que
+         numero
+      //).isNotNull()// Não é Nulo
+      ).isNull()// É Nulo
+      */
+      val resultado: Boolean? = true
+      assertThat(// Afirmar que
+         resultado
+      //).isTrue()
+      ).isFalse()
 
    }
 
