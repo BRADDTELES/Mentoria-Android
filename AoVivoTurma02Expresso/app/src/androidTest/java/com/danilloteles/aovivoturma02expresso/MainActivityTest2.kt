13b22fb965d2e140409e55c77f9855f88b87415b
BRADDTELES <braddteles@gmail.com>
2024-12-09 16:09:02 -0300

    Resumão do módulo (Testes Espresso) [Gravado ao vivo]

---
+++ AoVivoTurma02Expresso/app/src/androidTest/java/com/danilloteles/aovivoturma02expresso/MainActivityTest2.kt
@@ -0,0 +1,101 @@
+package com.danilloteles.aovivoturma02expresso
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withParent
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest2 {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun mainActivityTest2() {
+        val textInputEditText = onView(
+            allOf(
+                withId(R.id.editEmail),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.textInputLayout),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText.perform(replaceText("ja@gmail.com"), closeSoftKeyboard())
+
+        val materialButton = onView(
+            allOf(
+                withId(R.id.btnLogar), withText("Logar"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.main),
+                        childAtPosition(
+                            withId(android.R.id.content),
+                            0
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton.perform(click())
+
+        val textView = onView(
+            allOf(
+                withId(R.id.textDetalhe), withText("Jamilton Damasceno"),
+                withParent(
+                    allOf(
+                        withId(R.id.main),
+                        withParent(withId(android.R.id.content))
+                    )
+                ),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(withText("Jamilton Damasceno")))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
---
+++ AoVivoTurma02Expresso/app/src/main/java/com/danilloteles/aovivoturma02expresso/DetalhesActivity.kt
@@ -0,0 +1,11 @@
+package com.danilloteles.aovivoturma02expresso
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetalhesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhes)
+    }
+}
