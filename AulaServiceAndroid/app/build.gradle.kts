cb6a694d3d9f2651f83f495c5cf5e7c01c897910
BRADDTELES <braddteles@gmail.com>
2024-12-10 06:24:38 -0300

    Executando serviço em uma Thread

--- AulaServiceAndroid/app/build.gradle.kts
+++ AulaServiceAndroid/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
    namespace = "com.application.aulaserviceandroid"
-   compileSdk = 34
+   compileSdk = 35
 
    defaultConfig {
       applicationId = "com.application.aulaserviceandroid"
--- AulaServiceAndroid/app/src/main/java/com/application/aulaserviceandroid/MeuServico.kt
+++ AulaServiceAndroid/app/src/main/java/com/application/aulaserviceandroid/MeuServico.kt
@@ -3,6 +3,7 @@ package com.application.aulaserviceandroid
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.util.Log
 
 class MeuServico : Service() {
 
@@ -12,15 +13,29 @@ class MeuServico : Service() {
 
    override fun onCreate() {
       super.onCreate()
-
+      Log.i("servico_android", "onCreate")
    }
 
    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
       //CÓDIGOS DO SERVIÇO
+      val minhaThread = MinhaThread()
+      minhaThread.run()
       return super.onStartCommand(intent, flags, startId)
    }
 
+   inner class MinhaThread : Thread() {
+      override fun run() {
+         super.run()
+         repeat(15){ contador ->
+            sleep(2000)
+            Log.i("servico_android", "executando serviço: $contador")
+         }
+         stopSelf()//Parar o serviço
+      }
+   }
+
    override fun onDestroy() {//DESTRUI O SERVIÇO
+      Log.i("servico_android", "onDestroy")
       super.onDestroy()
    }
 
