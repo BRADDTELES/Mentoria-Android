d9ef1646459089db770be93228c3100ab17cb259
BRADDTELES <braddteles@gmail.com>
2024-12-12 14:12:36 -0300

    Criando broadcast na Activity

--- AulaServiceAndroid/app/src/main/java/com/application/aulaserviceandroid/ComunicacaoBroadcastActivity.kt
+++ AulaServiceAndroid/app/src/main/java/com/application/aulaserviceandroid/ComunicacaoBroadcastActivity.kt
@@ -1,11 +1,14 @@
 package com.application.aulaserviceandroid
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
 import android.net.wifi.WifiManager
 import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,6 +21,28 @@ class ComunicacaoBroadcastActivity : AppCompatActivity() {
       ActivityComunicacaoBroadcastBinding.inflate(layoutInflater)
    }
    private lateinit var comunicacaoBroadcastReceiver: ComunicacaoBroadcastReceiver
+
+   private val broadcastActivity = object : BroadcastReceiver(){
+      override fun onReceive(context: Context, intent: Intent?) {
+
+         if (  intent?.action == Intent.ACTION_POWER_CONNECTED  ) {
+            Toast.makeText(
+               applicationContext,
+               "Cabo energia conectado",
+               Toast.LENGTH_SHORT).show()
+
+            binding.textInformacao.text = "Cabo energia conectado"
+         }else{
+            Toast.makeText(
+               applicationContext,
+               "Cabo energia desconectado",
+               Toast.LENGTH_SHORT).show()
+
+            binding.textInformacao.text = "Cabo energia desconectado"
+         }
+      }
+
+   }
 
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
@@ -50,10 +75,24 @@ class ComunicacaoBroadcastActivity : AppCompatActivity() {
          }
       }
 
+      //Broadcast Activity
+      IntentFilter().apply {
+         addAction( Intent.ACTION_POWER_CONNECTED )
+         addAction( Intent.ACTION_POWER_DISCONNECTED )
+      }.also { intentFilter ->
+         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            registerReceiver( broadcastActivity, intentFilter, RECEIVER_NOT_EXPORTED)
+         }else{
+            registerReceiver( broadcastActivity, intentFilter)
+         }
+         //registerReceiver( broadcastActivity, intentFilter)
+      }
+
    }
 
    override fun onDestroy() {
       unregisterReceiver( comunicacaoBroadcastReceiver )
+      unregisterReceiver( broadcastActivity )
       super.onDestroy()
    }
 }
