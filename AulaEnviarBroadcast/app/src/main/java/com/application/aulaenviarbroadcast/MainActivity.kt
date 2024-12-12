576feb08876aea4787bbcc68f72afed4d24a57d6
BRADDTELES <braddteles@gmail.com>
2024-12-12 11:11:06 -0300

    Intent implícitas e Explícitas

--- AulaEnviarBroadcast/app/src/main/java/com/application/aulaenviarbroadcast/MainActivity.kt
+++ AulaEnviarBroadcast/app/src/main/java/com/application/aulaenviarbroadcast/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.application.aulaenviarbroadcast
 
+import android.content.ComponentName
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -19,7 +20,15 @@ class MainActivity : AppCompatActivity() {
       binding.btnAbrirPdf.setOnClickListener {
          val intent = Intent("com.application.aulaserviceandroid.ABRIR_ARQUIVO_PDF")
          intent.putExtra("ARQUIVO", "local/perfil.pdf") //Ação de Enviar o arquivo do PDF, com o nome do arquivo e o local aonde está o arquivo
+
+         val componente = ComponentName(
+            "com.application.aulaserviceandroid",
+            "com.application.aulaserviceandroid.ComunicacaoBroadcastActivity"
+         )
+         intent.component = componente
          sendBroadcast( intent )
+
+         //startActivity(Intent(this, MainActivity::class.java))
       }
 
    }
--- AulaServiceAndroid/app/src/main/java/com/application/aulaserviceandroid/ComunicacaoBroadcastActivity.kt
+++ AulaServiceAndroid/app/src/main/java/com/application/aulaserviceandroid/ComunicacaoBroadcastActivity.kt
@@ -2,26 +2,52 @@ package com.application.aulaserviceandroid
 
 import android.content.Intent
 import android.content.IntentFilter
+import android.net.Uri
 import android.net.wifi.WifiManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.application.aulaserviceandroid.databinding.ActivityComunicacaoBroadcastBinding
 
 class ComunicacaoBroadcastActivity : AppCompatActivity() {
 
+   private val binding by lazy {
+      ActivityComunicacaoBroadcastBinding.inflate(layoutInflater)
+   }
    private lateinit var comunicacaoBroadcastReceiver: ComunicacaoBroadcastReceiver
 
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
-      setContentView(R.layout.activity_comunicacao_broadcast)
+      setContentView( binding.root )
+
+      binding.btnImplicita.setOnClickListener {
+
+         /*val link = "https://www.google.com/"
+         val intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+         startActivity(intent)*/
+
+         val intent = Intent()
+         intent.action = Intent.ACTION_SEND
+         intent.putExtra(Intent.EXTRA_TEXT, "Olá, tudo bem ?")
+         intent.type = "plain/text"
+
+         val compartilhar = Intent.createChooser( intent, "Compartilhar")
+         startActivity(compartilhar)
+
+      }
 
       comunicacaoBroadcastReceiver = ComunicacaoBroadcastReceiver()
       IntentFilter(
          "com.application.aulaserviceandroid.ABRIR_ARQUIVO_PDF"
       ).apply {
-         registerReceiver( comunicacaoBroadcastReceiver, this )
+         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            registerReceiver( comunicacaoBroadcastReceiver, this, RECEIVER_NOT_EXPORTED)
+         }else{
+            registerReceiver( comunicacaoBroadcastReceiver, this)
+         }
       }
 
    }
