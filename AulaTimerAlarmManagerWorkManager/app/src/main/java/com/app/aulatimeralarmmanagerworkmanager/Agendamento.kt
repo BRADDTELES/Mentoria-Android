7dba3e148214ba309805f95c6f918db29796f7b4
BRADDTELES <braddteles@gmail.com>
2024-12-13 14:24:08 -0300

    Configurando data para o alarme

--- AulaTimerAlarmManagerWorkManager/app/src/main/java/com/app/aulatimeralarmmanagerworkmanager/Agendamento.kt
+++ AulaTimerAlarmManagerWorkManager/app/src/main/java/com/app/aulatimeralarmmanagerworkmanager/Agendamento.kt
@@ -4,6 +4,7 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.icu.util.Calendar
 
 class Agendamento(
    private val context: Context
@@ -15,10 +16,22 @@ class Agendamento(
 
       val intent = Intent(context, AgendamentoBroadcastReceiver::class.java)
 
+      val calendar = Calendar.getInstance().apply {
+         timeInMillis = System.currentTimeMillis()// 20/10/2025 10:20:30
+         //set(2025, 0, 10)
+         /*set(Calendar.YEAR, 2025)
+         set(Calendar.MONTH, 0)
+         set(Calendar.HOUR_OF_DAY, 22)*/
+         //add(Calendar.YEAR, 1)// 20/10/2026 10:20:30
+         //add(Calendar.SECOND, 10)// 20/10/2025 10:20:40
+         set(Calendar.HOUR_OF_DAY, 11)
+         set(Calendar.MINUTE, 20)
+      }
+
       alarmeManager = context.getSystemService( AlarmManager::class.java )
       alarmeManager.set(
          AlarmManager.RTC_WAKEUP,//Data e hora para execução
-         System.currentTimeMillis() + 20000,
+         calendar.timeInMillis,
          PendingIntent.getBroadcast(
             context,
             1,
