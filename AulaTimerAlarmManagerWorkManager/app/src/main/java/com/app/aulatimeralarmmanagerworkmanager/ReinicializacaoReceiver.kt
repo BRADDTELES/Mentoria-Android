a6476f6b2ead50b7bf1be0b80951f6d3da10de86
BRADDTELES <braddteles@gmail.com>
2024-12-13 16:45:41 -0300

    Agendando alarmes na reinicialização

--- AulaTimerAlarmManagerWorkManager/app/src/main/java/com/app/aulatimeralarmmanagerworkmanager/ReinicializacaoReceiver.kt
+++ AulaTimerAlarmManagerWorkManager/app/src/main/java/com/app/aulatimeralarmmanagerworkmanager/ReinicializacaoReceiver.kt
@@ -10,6 +10,8 @@ class ReinicializacaoReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         if (  intent.action == Intent.ACTION_BOOT_COMPLETED  ) {
             Log.i("agendamento_android", "reinicializado")
+            val agendamento = Agendamento(context)
+            agendamento.agendar()
         }
     }
 }
