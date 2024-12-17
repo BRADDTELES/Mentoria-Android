3a66bf565e8efea74d6dfef5591e9b04e74dc148
BRADDTELES <braddteles@gmail.com>
2024-12-17 08:51:26 -0300

    Correções mergiada na main

--- AulaoGitAndroidStudio/app/src/main/java/com/danilloteles/aulaogitandroidstudio/PagamentoActivity.kt
+++ AulaoGitAndroidStudio/app/src/main/java/com/danilloteles/aulaogitandroidstudio/PagamentoActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class PagementoActivity : AppCompatActivity() {
+class PagamentoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
