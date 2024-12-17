e5d60587fad298092e67461df362ba8a07dc08f7
BRADDTELES <braddteles@gmail.com>
2024-12-17 05:52:07 -0300

    Reapply "Adicionado envio de imagens no chat"
    This reverts commit c9511cd9e1653e81b02604b81684063d6085ec25.

--- AulaoGitAndroidStudio/app/src/main/java/com/danilloteles/aulaogitandroidstudio/EnvioImagemActivity.kt
+++ AulaoGitAndroidStudio/app/src/main/java/com/danilloteles/aulaogitandroidstudio/EnvioImagemActivity.kt
@@ -6,22 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class ChatActivity : AppCompatActivity() {
+class EnvioImagemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_chat)
+        setContentView(R.layout.activity_envio_imagem)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
-
-    fun enviarMensagem() {
-
-    }
-
-    fun enviarImagem() {}
-
 }
