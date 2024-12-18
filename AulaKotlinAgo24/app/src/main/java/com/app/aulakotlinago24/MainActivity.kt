a1624f14e224f3aa6efecfb8b8b2db0135ec0bd4
BRADDTELES <braddteles@gmail.com>
2024-12-18 10:31:31 -0300

    Resumão de kotlin: lateinit

--- AulaKotlinAgo24/app/src/main/java/com/app/aulakotlinago24/MainActivity.kt
+++ AulaKotlinAgo24/app/src/main/java/com/app/aulakotlinago24/MainActivity.kt
@@ -8,13 +8,13 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
-
+    //lateinit -> inicialização tardia -> Carrega recursos que só podem ser executado quando onCreate for chamado dos ( Preferencias )
+    /* lateinit var é um modificador em Kotlin
+    que permite que você declare uma propriedade não nula que será inicializada mais tarde */
+    lateinit var bancoDados: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
     }
 }
