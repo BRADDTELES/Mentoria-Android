29b8b399103f34802f8adccbe031980becc68059
BRADDTELES <braddteles@gmail.com>
2024-12-18 11:02:02 -0300

    Resumão do módulo (Aprenda Kotlin do Zero) - Variáveis e constantes

--- RevisaoAprendaKotlinZero/app/build.gradle.kts
+++ RevisaoAprendaKotlinZero/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.app.revisaoaprendakotlinzero"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.app.revisaoaprendakotlinzero"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
---
+++ RevisaoAprendaKotlinZero/app/src/main/java/com/app/revisaoaprendakotlinzero/RevisaoKotlin.kt
@@ -0,0 +1,32 @@
+package com.app.revisaoaprendakotlinzero
+
+/*
+Resumão Aprenda Kotlin do zero
++ Variáveis e constantes
++ Null Safety
++ Operadores aritiméticos, relacionais e lógicos
++ if else when
++ Arrays & Loops: while e for
++ Funções e parâmetros
++ Classes, objetos, atributos e métodos
++ Sobrecarga métodos
++ Modificadores de acesso (public, private, protected)
++ Herança
++ Interface
++ Companion Object
++ Lateinit
+* */
+
+const val pi = 3.14
+
+fun main() {
+
+    //val nome = "Danillo"//Variável imutável
+    var nome = "Danillo"//Variável mutável
+
+    nome = "Maria"
+
+    println( nome )
+    println( pi )
+
+}
