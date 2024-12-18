1d8788854b16422f27da0c6c640a5e26470e9e80
BRADDTELES <braddteles@gmail.com>
2024-12-18 10:37:30 -0300

    Inicializando o Resumão do módulo (Aprenda Kotlin do Zero)

---
+++ RevisaoAprendaKotlinZero/app/src/main/java/com/app/revisaoaprendakotlinzero/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.app.revisaoaprendakotlinzero
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/*
+Resumão Aprenda Kotlin do zero
++ Variáveis e constantes
++ Null Safety
++ Operadores aritiméticos, relacionais e lógicos
++ if else when
++ Arrays & Loops: while e for
++ Funções e parâmetros
++ Classes, objetos, atributos e métodos
++ Sobrecarga métodos
++ Modificadores de acesso (public, private, protected)
++ Herança
++ Interface
++ Companion Object
++ Lateinit
+* */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
