067bbcfbbbf81b8f0b7c410aeb16829c8f9bcb08
BRADDTELES <braddteles@gmail.com>
2024-12-24 06:34:33 -0300

    Clean Code & Solid - Prática - DIP - Princípio da inversão de dependência

--- AulaCleanESolid/app/src/main/java/com/danilloteles/aulacleanesolid/DIP.kt
+++ AulaCleanESolid/app/src/main/java/com/danilloteles/aulacleanesolid/DIP.kt
@@ -1,13 +1,31 @@
 package com.danilloteles.aulacleanesolid
 
-class BancoDados {//Baixo nivel
-    fun conectar() {
+/*
+classes não devem depender de outras classes concretas
+* */
+
+
+//Concreta
+class BancoDados : ConectorBancoDados {//Baixo nivel
+    override fun conectar() {
         println("conectado banco de dados")
     }
 }
 
+class BancoDadosOracle : ConectorBancoDados {//Baixo nivel
+    override fun conectar() {
+        println("conectado banco de dados")
+    }
+}
+
+//Abstração
+interface ConectorBancoDados {
+    fun conectar()
+}
+
+//Concreta
 class Usuario(//Alto nivel
-    private val bancoDados: BancoDados
+    private val conectorBancoDados: ConectorBancoDados //Injeção de dependencia / Inversao de dependencia
 ) {
 
     fun enviarEmailRecuperacaoSenha() {
@@ -27,6 +45,9 @@ class Usuario(//Alto nivel
 
 fun main() {
 
+    val bancoDados = BancoDados()
+    val bancoDadosOracle = BancoDadosOracle()
 
+    val usuario = Usuario( bancoDadosOracle )
 
 }
