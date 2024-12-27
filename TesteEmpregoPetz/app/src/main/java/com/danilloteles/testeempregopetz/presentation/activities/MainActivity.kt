1d3ef0dafddb2c4a7f1747f677682e061a59df05
BRADDTELES <braddteles@gmail.com>
2024-12-27 06:08:22 -0300

    #Teste de Emprego com MVVM e Hilt - Editando o activity_main com o RecycleView das Cartas

--- TesteEmpregoPetz/app/src/main/java/com/danilloteles/testeempregopetz/presentation/activities/MainActivity.kt
+++ TesteEmpregoPetz/app/src/main/java/com/danilloteles/testeempregopetz/presentation/activities/MainActivity.kt
@@ -3,12 +3,18 @@ package com.danilloteles.testeempregopetz.presentation.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.danilloteles.testeempregopetz.R
+import com.danilloteles.testeempregopetz.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView( binding.root )
     }
 }
