e579e91c3d21d81a2e2837d7250c19af47937b81
BRADDTELES <braddteles@gmail.com>
2025-01-03 08:27:36 -0300

    Criando uma entidade (Entity)

---
+++ AulaRoomDatabase/app/src/main/java/com/danilloteles/aularoomdatabase/data/model/Usuario.kt
@@ -0,0 +1,20 @@
+package com.danilloteles.aularoomdatabase.data.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "usuarios")
+class Usuario(
+    @PrimaryKey(autoGenerate = true)
+    val id: String,
+    val email: String,
+    @ColumnInfo("nome_sobrenome")
+    val nomeSobrenome: String,
+    val senha: String,
+    val idade: Int,
+    val peso: Double,
+    //@Ignore val imc: Double
+) {
+}
