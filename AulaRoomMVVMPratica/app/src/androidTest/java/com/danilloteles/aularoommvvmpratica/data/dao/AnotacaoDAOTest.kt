472c2618b519d29635c65964e553cff5a1f9dede
BRADDTELES <braddteles@gmail.com>
2025-01-05 08:51:23 -0300

    Testando AnotacaoDAO

--- AulaRoomMVVMPratica/app/src/androidTest/java/com/danilloteles/aularoommvvmpratica/data/dao/AnotacaoDAOTest.kt
+++ AulaRoomMVVMPratica/app/src/androidTest/java/com/danilloteles/aularoommvvmpratica/data/dao/AnotacaoDAOTest.kt
@@ -3,6 +3,7 @@ package com.danilloteles.aularoommvvmpratica.data.dao
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import com.danilloteles.aularoommvvmpratica.data.database.BancoDados
+import com.danilloteles.aularoommvvmpratica.data.entity.Anotacao
 import com.danilloteles.aularoommvvmpratica.data.entity.Categoria
 import com.google.common.truth.Truth.assertThat
 import org.junit.After
@@ -13,6 +14,7 @@ class AnotacaoDAOTest {
 
   private lateinit var bancoDados: BancoDados
   private lateinit var categoriaDAO: CategoriaDAO
+  private lateinit var anotacaoDAO: AnotacaoDAO
 
   @Before
   fun setUp() {
@@ -21,6 +23,7 @@ class AnotacaoDAOTest {
       BancoDados::class.java
     ).allowMainThreadQueries().build()
     categoriaDAO = bancoDados.categoriaDAO
+    anotacaoDAO = bancoDados.anotacaoDAO
   }
 
   @Test
@@ -28,6 +31,23 @@ class AnotacaoDAOTest {
     val categoria = Categoria(0, "mercado")
     val idCategoria = categoriaDAO.salvar( categoria )
     assertThat( idCategoria ).isGreaterThan(0L)
+  }
+
+  @Test
+  fun salvarAnotacao_verificaAnotacaoCadastrada_retornaTrue() {
+    salvarCategoria_verificaCategoriaCadastrada_retornaTrue()
+    val anotacao = Anotacao(
+      0, 1, "Titulo", "descrição"
+    )
+    val idAnotacao = anotacaoDAO.salvar( anotacao )
+    assertThat( idAnotacao ).isGreaterThan( 0L)
+  }
+
+  @Test
+  fun listarAnotacao_verificaListagemAnotacoes_retornaTrue() {
+    salvarAnotacao_verificaAnotacaoCadastrada_retornaTrue()
+    val listaAnotacoes = anotacaoDAO.listar()
+    assertThat( listaAnotacoes ).isNotEmpty()
   }
 
   @After
