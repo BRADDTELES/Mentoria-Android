612de7eea34f6db18a6626963d0f3e7d035ac665
BRADDTELES <braddteles@gmail.com>
2025-01-05 09:34:11 -0300

    Criando observador e observável

--- AulaRXJava/app/src/main/java/com/danilloteles/aularxjava/MainActivity.kt
+++ AulaRXJava/app/src/main/java/com/danilloteles/aularxjava/MainActivity.kt
@@ -1,20 +1,56 @@
 package com.danilloteles.aularxjava
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.danilloteles.aularxjava.databinding.ActivityMainBinding
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
 
 class MainActivity : AppCompatActivity() {
+
+  private val binding by lazy {
+    ActivityMainBinding.inflate( layoutInflater )
+  }
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    enableEdgeToEdge()
-    setContentView(R.layout.activity_main)
-    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-      insets
+    setContentView( binding.root )
+
+    criarObersavadorObservavel()
+
+  }
+
+  private fun criarObersavadorObservavel() {
+
+    //Observável - ViewModel
+    val listaNomes = listOf("Jamilton", "Ana", "Maria")//API, Banco dados
+    val obersavel = Observable.fromIterable( listaNomes )
+
+    //Observador - Activity
+    val observador = object : Observer<String> {
+      override fun onSubscribe(d: Disposable) {
+        Log.i("info_rxjava","onSubscribe: inicio execução")
+      }
+
+      override fun onNext(item: String) {
+        Log.i("info_rxjava","onNext - Nome: $item")
+      }
+
+      override fun onError(e: Throwable) {
+        Log.i("info_rxjava","onError: ${e.message}")
+      }
+
+      override fun onComplete() {
+        Log.i("info_rxjava","onComplete: fim execução")
+      }
+
     }
+    
+    obersavel.subscribe( observador )
+
   }
 }
