70f2307c6119d10a5d15c6beff3f1c4ebc501884
BRADDTELES <braddteles@gmail.com>
2025-01-05 08:39:44 -0300

    Testando CategoriaDAO

--- AulaRoomMVVMPratica/app/src/androidTest/java/com/danilloteles/aularoommvvmpratica/data/dao/AnotacaoDAOTest.kt
+++ AulaRoomMVVMPratica/app/src/androidTest/java/com/danilloteles/aularoommvvmpratica/data/dao/AnotacaoDAOTest.kt
@@ -3,12 +3,16 @@ package com.danilloteles.aularoommvvmpratica.data.dao
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import com.danilloteles.aularoommvvmpratica.data.database.BancoDados
+import com.danilloteles.aularoommvvmpratica.data.entity.Categoria
+import com.google.common.truth.Truth.assertThat
 import org.junit.After
 import org.junit.Before
+import org.junit.Test
 
 class AnotacaoDAOTest {
 
   private lateinit var bancoDados: BancoDados
+  private lateinit var categoriaDAO: CategoriaDAO
 
   @Before
   fun setUp() {
@@ -16,6 +20,14 @@ class AnotacaoDAOTest {
       ApplicationProvider.getApplicationContext(),
       BancoDados::class.java
     ).allowMainThreadQueries().build()
+    categoriaDAO = bancoDados.categoriaDAO
+  }
+
+  @Test
+  fun salvarCategoria_verificaCategoriaCadastrada_retornaTrue() {
+    val categoria = Categoria(0, "mercado")
+    val idCategoria = categoriaDAO.salvar( categoria )
+    assertThat( idCategoria ).isGreaterThan(0L)
   }
 
   @After
