603f2668a71c1c785a2d25e642706d6212a0c025
BRADDTELES <braddteles@gmail.com>
2025-01-06 06:41:37 -0300

    Utilizando modificadores

--- AulaJetpackCompose/app/src/main/java/com/danilloteles/aulajetpackcompose/MainActivity.kt
+++ AulaJetpackCompose/app/src/main/java/com/danilloteles/aulajetpackcompose/MainActivity.kt
@@ -5,21 +5,29 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.danilloteles.aulajetpackcompose.componentes.Botao
 import com.danilloteles.aulajetpackcompose.ui.theme.AulaJetpackComposeTheme
@@ -40,7 +48,40 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun PrimeiroApp() {
 
-   Column (
+   Column(
+      modifier = Modifier
+         /*.width(200.dp)
+         .height(200.dp)*/
+         .background(Color.Gray)
+         .border(2.dp, Color.Red)
+         //.padding(30.dp, 10.dp)
+         .padding(30.dp)
+         //.padding(top = 30.dp, end = 30.dp)
+         .fillMaxWidth()
+         .fillMaxHeight()
+   ) {
+      Text(text = "Jamilton", fontSize = 20.sp)
+
+      Column(
+         modifier = Modifier
+            .width(100.dp)
+            .height(100.dp)
+            //.clip( CircleShape )
+            //.clip( RoundedCornerShape( topStart = 20.dp, topEnd = 8.dp, bottomEnd = 20.dp, bottomStart = 8.dp ) )
+            //.clip( CutCornerShape( topStart = 20.dp, topEnd = 8.dp, bottomEnd = 20.dp, bottomStart = 8.dp ) )
+            //.clip( CutCornerShape( 8.dp ) )
+            .border(2.dp, Color.Red, CircleShape )
+            .clip( CircleShape )
+            .background(Color.Green),
+         horizontalAlignment = Alignment.CenterHorizontally,
+         verticalArrangement = Arrangement.Center
+            //.padding(top = 10.dp, end = 30.dp, bottom = 30.dp, start = 8.dp)
+      ) {
+         Text(text = "JM", fontSize = 32.sp)
+      }
+   }
+
+   /*Column (
       modifier = Modifier
          .fillMaxWidth()
          .fillMaxHeight()
@@ -57,7 +98,7 @@ fun PrimeiroApp() {
          println("executou")
       }
 
-   } //Fim Column
+   }*/ //Fim Column
 
    /*Text(
       text = "Danillo",
--- AulaJetpackCompose/app/src/main/java/com/danilloteles/aulajetpackcompose/componentes/Botao.kt
+++ AulaJetpackCompose/app/src/main/java/com/danilloteles/aulajetpackcompose/componentes/Botao.kt
@@ -1,11 +1,22 @@
 package com.danilloteles.aulajetpackcompose.componentes
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun Botao (
@@ -22,6 +33,22 @@ fun Botao (
    }
 
 }
+
+/*@Composable
+fun PerfilUsuario(){
+   Column(
+      modifier = Modifier
+         .width(100.dp)
+         .height(100.dp)
+         .clip(CircleShape)
+         .background(Color.Green),
+      horizontalAlignment = Alignment.CenterHorizontally,
+      verticalArrangement = Arrangement.Center
+      //.padding(top = 10.dp, end = 30.dp, bottom = 30.dp, start = 8.dp)
+   ) {
+      Text(text = "JM", fontSize = 32.sp)
+   }
+}*/
 
 @Preview
 @Composable
