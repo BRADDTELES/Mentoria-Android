5ced51686091f8140e5793f7507c288b0a4efb76
BRADDTELES <braddteles@gmail.com>
2025-01-06 07:17:34 -0300

    Trabalhando com listas de dados - LazyColumn

--- AulaJetpackCompose/app/src/main/java/com/danilloteles/aulajetpackcompose/MainActivity.kt
+++ AulaJetpackCompose/app/src/main/java/com/danilloteles/aulajetpackcompose/MainActivity.kt
@@ -16,6 +16,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -38,9 +40,38 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.danilloteles.aulajetpackcompose.componentes.Botao
+import com.danilloteles.aulajetpackcompose.model.Usuario
 import com.danilloteles.aulajetpackcompose.ui.theme.AulaJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
+
+   private val usuarios = listOf(
+      Usuario("jamilton", 36),
+      Usuario("ana", 45),
+      Usuario("Maria", 35),
+      Usuario("João", 22),
+      Usuario("Pedro", 16),
+      Usuario("Tiago", 65),
+      Usuario("Joana", 70),
+      Usuario("...", 19),
+      Usuario("...", 19),
+      Usuario("...", 19),
+      Usuario("...", 19),
+      Usuario("...", 19),
+      Usuario("...", 19),
+      Usuario("...", 19),
+      Usuario("...", 19),
+      Usuario("...", 19),
+      Usuario("...", 19),
+      Usuario("...", 19),
+      Usuario("...", 19),
+      Usuario("...", 19),
+      Usuario("...", 19),
+      Usuario("...", 19),
+      Usuario("...", 19),
+      Usuario("...", 19),
+   )
+
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
       enableEdgeToEdge()
@@ -49,45 +80,73 @@ class MainActivity : ComponentActivity() {
             PrimeiroApp()
          }//Fechamento theme
       }
-   }
+   }//Fim OnCreate
 
-}//fechamento MainActivity
+   @Composable
+   fun PrimeiroApp() {
 
-@Composable
-fun PrimeiroApp() {
-
-   Column(
-      modifier = Modifier
-         /*.width(200.dp)
-         .height(200.dp)*/
-         .background(Color.Gray)
-         .border(2.dp, Color.Red)
-         //.padding(30.dp, 10.dp)
-         .padding(30.dp)
-         //.padding(top = 30.dp, end = 30.dp)
-         .fillMaxWidth()
-         .fillMaxHeight()
-   ) {
-
-      Image(
-         painter = painterResource(id = R.drawable.carro),
-         contentDescription = null,
+      Column(
          modifier = Modifier
-            .height(200.dp)
-            .width(200.dp)
-            .border(2.dp, Color.Red),
-         contentScale = ContentScale.Crop,
-         //alignment = Alignment.TopCenter
-      )
+            /*.width(200.dp)
+            .height(200.dp)*/
+            .background(Color.Gray)
+            .border(2.dp, Color.Red)
+            //.padding(30.dp, 10.dp)
+            .padding(30.dp)
+            //.padding(top = 30.dp, end = 30.dp)
+            .fillMaxWidth()
+            .fillMaxHeight()
+      ) {
 
-      Icon(
-         //painter = painterResource(id = R.drawable.ic_alarme_24),
-         imageVector = Icons.Default.Lock,
-         contentDescription = null,
-         /*modifier = Modifier
+         LazyColumn(
+            modifier = Modifier
+               .padding(16.dp)
+         ) {
+            items( usuarios.size ){ indice ->
+
+               val nome = usuarios[indice].nome
+               val idade = usuarios[indice].idade
+
+               Text(
+                  text = "$nome - $idade",
+                  fontSize = 32.sp
+               )
+
+            }
+         }
+
+         /*for (i in 1..4){
+            Image(
+               painter = painterResource(id = R.drawable.carro),
+               contentDescription = null,
+               modifier = Modifier
+                  .height(200.dp)
+                  .width(200.dp)
+                  .border(2.dp, Color.Red),
+               contentScale = ContentScale.Crop,
+               //alignment = Alignment.TopCenter
+            )
+         }*/
+
+         /*Image(
+            painter = painterResource(id = R.drawable.carro),
+            contentDescription = null,
+            modifier = Modifier
+               .height(200.dp)
+               .width(200.dp)
+               .border(2.dp, Color.Red),
+            contentScale = ContentScale.Crop,
+            //alignment = Alignment.TopCenter
+         )
+
+         Icon(
+            //painter = painterResource(id = R.drawable.ic_alarme_24),
+            imageVector = Icons.Default.Lock,
+            contentDescription = null,
+            *//*modifier = Modifier
             .height(200.dp)
             .width(200.dp)
-            .border(2.dp, Color.Red)*/
+            .border(2.dp, Color.Red)*//*
       )
 
       Button(onClick = {}) {
@@ -107,62 +166,66 @@ fun PrimeiroApp() {
             }
             Text(text = "Desbloquear")
          }
-      }
-
-      /*Text(text = "Jamilton", fontSize = 20.sp)
-
-      Column(
-         modifier = Modifier
-            .width(100.dp)
-            .height(100.dp)
-            //.clip( CircleShape )
-            //.clip( RoundedCornerShape( topStart = 20.dp, topEnd = 8.dp, bottomEnd = 20.dp, bottomStart = 8.dp ) )
-            //.clip( CutCornerShape( topStart = 20.dp, topEnd = 8.dp, bottomEnd = 20.dp, bottomStart = 8.dp ) )
-            //.clip( CutCornerShape( 8.dp ) )
-            .border(2.dp, Color.Red, CircleShape )
-            .clip( CircleShape )
-            .background(Color.Green),
-         horizontalAlignment = Alignment.CenterHorizontally,
-         verticalArrangement = Arrangement.Center
-            //.padding(top = 10.dp, end = 30.dp, bottom = 30.dp, start = 8.dp)
-      ) {
-         Text(text = "JM", fontSize = 32.sp)
       }*/
-   }//Fim Column
 
-   /*Column (
-      modifier = Modifier
-         .fillMaxWidth()
-         .fillMaxHeight()
-         .background(Color.Gray),
-      verticalArrangement = Arrangement.SpaceAround,
-      horizontalAlignment = Alignment.CenterHorizontally
-      //horizontalArrangement = Arrangement.SpaceEvenly,
-      //verticalAlignment = Alignment.CenterVertically
-   ) {
+         /*Text(text = "Jamilton", fontSize = 20.sp)
 
-      Text(text = "Jamilton", fontSize = 20.sp)
-      Text(text = "Ana", fontSize = 20.sp)
-      Botao(texto = "Jamilton") {
-         println("executou")
-      }
+         Column(
+            modifier = Modifier
+               .width(100.dp)
+               .height(100.dp)
+               //.clip( CircleShape )
+               //.clip( RoundedCornerShape( topStart = 20.dp, topEnd = 8.dp, bottomEnd = 20.dp, bottomStart = 8.dp ) )
+               //.clip( CutCornerShape( topStart = 20.dp, topEnd = 8.dp, bottomEnd = 20.dp, bottomStart = 8.dp ) )
+               //.clip( CutCornerShape( 8.dp ) )
+               .border(2.dp, Color.Red, CircleShape )
+               .clip( CircleShape )
+               .background(Color.Green),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+               //.padding(top = 10.dp, end = 30.dp, bottom = 30.dp, start = 8.dp)
+         ) {
+            Text(text = "JM", fontSize = 32.sp)
+         }*/
 
-   }*/ //Fim Column
+      }//Fim Column
 
-   /*Text(
-      text = "Danillo",
-      color = Color.White
-   )*/
-   /*Button(onClick = {  }) {
-      Text(
+      /*Column (
+         modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .background(Color.Gray),
+         verticalArrangement = Arrangement.SpaceAround,
+         horizontalAlignment = Alignment.CenterHorizontally
+         //horizontalArrangement = Arrangement.SpaceEvenly,
+         //verticalAlignment = Alignment.CenterVertically
+      ) {
+
+         Text(text = "Jamilton", fontSize = 20.sp)
+         Text(text = "Ana", fontSize = 20.sp)
+         Botao(texto = "Jamilton") {
+            println("executou")
+         }
+
+      }*/ //Fim Column
+
+      /*Text(
          text = "Danillo",
          color = Color.White
-      )
-   }*/
-}
+      )*/
+      /*Button(onClick = {  }) {
+         Text(
+            text = "Danillo",
+            color = Color.White
+         )
+      }*/
+   }
 
-@Preview
-@Composable
-fun PrimeiroAppPreview() {
-   PrimeiroApp()
-}
+   @Preview
+   @Composable
+   fun PrimeiroAppPreview() {
+      PrimeiroApp()
+   }
+
+}//fechamento MainActivity
+
---
+++ AulaJetpackCompose/app/src/main/java/com/danilloteles/aulajetpackcompose/model/Usuario.kt
@@ -0,0 +1,6 @@
+package com.danilloteles.aulajetpackcompose.model
+
+data class Usuario(
+   val nome: String,
+   val idade: Int
+)
