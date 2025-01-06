2e0e47ac3676a56c1b299b503ff30fe41073e4d2
BRADDTELES <braddteles@gmail.com>
2025-01-06 08:46:43 -0300

    Componentes interface: FAB, CheckBox, Switch, Radio

--- AulaJetpackCompose/app/src/main/java/com/danilloteles/aulajetpackcompose/MainActivity.kt
+++ AulaJetpackCompose/app/src/main/java/com/danilloteles/aulajetpackcompose/MainActivity.kt
@@ -1,45 +1,23 @@
 package com.danilloteles.aulajetpackcompose
 
-import android.inputmethodservice.Keyboard.Row
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.CutCornerShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material3.Button
-import androidx.compose.material3.Divider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -47,14 +25,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.danilloteles.aulajetpackcompose.componentes.Botao
 import com.danilloteles.aulajetpackcompose.model.Usuario
 import com.danilloteles.aulajetpackcompose.ui.theme.AulaJetpackComposeTheme
 
@@ -87,6 +62,10 @@ class MainActivity : ComponentActivity() {
       Usuario("...", 19),
    )
 
+   private val opcoesRadio = listOf(
+      "Android", "iOS", "Flutter", "React Native"
+   )
+
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
       enableEdgeToEdge()
@@ -112,6 +91,14 @@ class MainActivity : ComponentActivity() {
          mutableStateOf( listOf<Usuario>() )
       }
 
+      var checked by remember {
+         mutableStateOf( false )
+      }
+
+      var radioSelecionado by remember {
+         mutableStateOf( opcoesRadio[0] )
+      }
+
       Column(
          modifier = Modifier
             .background(Color.White)
@@ -120,9 +107,50 @@ class MainActivity : ComponentActivity() {
             .fillMaxHeight()
       ) {
 
-         Row(
+         opcoesRadio.forEach { opcao ->
 
+            Row(
+               verticalAlignment = Alignment.CenterVertically
+            ) {
+               RadioButton(
+                  selected = opcao == radioSelecionado,
+                  onClick = {
+                     radioSelecionado = opcao
+                  })
+               Text(text = opcao)
+            }
+
+         }
+         Text(text = "alterado: $radioSelecionado")
+
+         //Mais componentes de interface: FAB, CheckBox, Switch, Radio
+
+         //Switch(
+         /*Checkbox(
+            checked = checked,
+            onCheckedChange = { alterado ->
+               checked = alterado
+            }
+         )*/
+
+         //Text(text = "alterado: $checked")
+
+         //FloatingActionButton(
+         /*ExtendedFloatingActionButton(
+            *//*containerColor = Color.Magenta,
+            contentColor = Color.White,*//*
+            onClick = {
+
+            }
          ) {
+            *//*Icon(
+               painter = painterResource(id = R.drawable.ic_adicionar_24),
+               contentDescription = null
+            )*//*
+            Text(text = "Pesquisar")
+         }*/
+
+         /*Row {
             //TextField(
             OutlinedTextField(
                value = nome,
@@ -147,9 +175,9 @@ class MainActivity : ComponentActivity() {
                   contentDescription = null
                )
             }
-         }//Fim Row
+         }*///Fim Row
 
-         LazyColumn(
+         /*LazyColumn(
             modifier = Modifier
                .padding(top = 16.dp, bottom = 16.dp)
          ) {
@@ -160,7 +188,7 @@ class MainActivity : ComponentActivity() {
                )
                Divider()
             }
-         }
+         }*/
 
          //Text(text = "Contador: ${contador}")
          //Text(text = "nome: ${nome}")
