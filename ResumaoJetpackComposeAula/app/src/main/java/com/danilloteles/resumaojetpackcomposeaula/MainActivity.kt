c39d350184a0a1bb849dce124903b5ee60e98650
BRADDTELES <braddteles@gmail.com>
2025-01-07 12:03:40 -0300

    Resumão (Jetpack Compose) - Alinhamentos para Colunas e Linhas, usando também o clip

--- ResumaoJetpackComposeAula/app/src/main/java/com/danilloteles/resumaojetpackcomposeaula/MainActivity.kt
+++ ResumaoJetpackComposeAula/app/src/main/java/com/danilloteles/resumaojetpackcomposeaula/MainActivity.kt
@@ -5,13 +5,29 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -30,9 +46,72 @@ class MainActivity : ComponentActivity() {
 
    @Composable
    fun Home() {
+
+      //Row(
+      Column(
+         modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .border(2.dp, Color.Red)
+            .padding(top = 50.dp)
+            .background(Color.Magenta),
+         verticalArrangement = Arrangement.SpaceEvenly,
+         horizontalAlignment = Alignment.CenterHorizontally
+         /* Para Row
+         verticalAlignment = Alignment.CenterVertically,
+         horizontalArrangement = Arrangement.SpaceEvenly*/
+      ) {
+
+         Column(
+            modifier = Modifier
+               .width(100.dp)
+               .height(100.dp)
+               .background( Color.Blue, CircleShape ),
+               //.background( Color.Blue, RoundedCornerShape(20.dp) ),
+               //.background( Color.Blue, CutCornerShape(20.dp, 10.dp, 20.dp, 10.dp) ),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+         ) {
+            Text(
+               text = "JM",
+               fontSize = 20.sp,
+            )
+         }
+
+         Text(
+            modifier = Modifier
+               .padding(top = 5.dp),
+            text = "Danillo",
+            fontSize = 20.sp,
+         )
+         Text(
+            text = "Jamilton",
+            fontSize = 20.sp,
+         )
+         Text(
+            text = "Ana",
+            fontSize = 20.sp,
+         )
+      }
+
+      /*
+
+      Row(
+
+         ) {
+            Text(
+               text = "lado",
+               fontSize = 20.sp,
+            )
+            Text(
+               text = "lado",
+               fontSize = 20.sp,
+            )
+         }
+
       Button(
          modifier = Modifier
-            .padding(top = 40.dp),
+            .padding(top = 45.dp),
          onClick = {
          Toast.makeText(applicationContext, "Clicado", Toast.LENGTH_SHORT).show()
       }) {
@@ -40,7 +119,7 @@ class MainActivity : ComponentActivity() {
             text = "Danillo",
             fontSize = 20.sp,
          )
-      }
+      }*/
    }
 
    @Preview(showBackground = true)
