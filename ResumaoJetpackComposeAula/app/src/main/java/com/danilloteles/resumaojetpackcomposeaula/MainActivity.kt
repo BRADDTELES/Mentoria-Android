825ee3810200b7d57b6fc75dc4aabaca4ccb289e
BRADDTELES <braddteles@gmail.com>
2025-01-07 11:19:28 -0300

    Resumão (Jetpack Compose) - Editando e alinhando o Button e adicionando outros componenste, no caso o Texto

--- ResumaoJetpackComposeAula/app/src/main/java/com/danilloteles/resumaojetpackcomposeaula/MainActivity.kt
+++ ResumaoJetpackComposeAula/app/src/main/java/com/danilloteles/resumaojetpackcomposeaula/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.danilloteles.resumaojetpackcomposeaula
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.danilloteles.resumaojetpackcomposeaula.ui.theme.ResumaoJetpackComposeAulaTheme
 
@@ -27,10 +30,17 @@ class MainActivity : ComponentActivity() {
 
    @Composable
    fun Home() {
-      Text(
-         text = "Danillo",
-         fontSize = 20.sp,
-      )
+      Button(
+         modifier = Modifier
+            .padding(top = 40.dp),
+         onClick = {
+         Toast.makeText(applicationContext, "Clicado", Toast.LENGTH_SHORT).show()
+      }) {
+         Text(
+            text = "Danillo",
+            fontSize = 20.sp,
+         )
+      }
    }
 
    @Preview(showBackground = true)
