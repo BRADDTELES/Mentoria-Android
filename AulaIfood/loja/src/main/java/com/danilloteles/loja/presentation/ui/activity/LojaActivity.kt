98dc5026dc2ea964bcc51217626bf860f9525aa5
BRADDTELES <braddteles@gmail.com>
2025-01-10 09:18:29 -0300

    [Loja] Interface de cadastro

--- AulaIfood/loja/src/main/java/com/danilloteles/loja/presentation/ui/activity/LojaActivity.kt
+++ AulaIfood/loja/src/main/java/com/danilloteles/loja/presentation/ui/activity/LojaActivity.kt
@@ -2,13 +2,47 @@ package com.danilloteles.loja.presentation.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.danilloteles.core.AlertaCarregamento
+import com.danilloteles.core.navegarPara
 import com.danilloteles.loja.R
+import com.danilloteles.loja.databinding.ActivityCadastroBinding
+import com.danilloteles.loja.databinding.ActivityLoginBinding
+import com.danilloteles.loja.databinding.ActivityLojaBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LojaActivity : AppCompatActivity() {
+
+   private val binding by lazy {
+      ActivityLojaBinding.inflate(layoutInflater)
+   }
+   private val alertaCarregamento by lazy {
+      AlertaCarregamento(this)
+   }
+
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
-      setContentView(R.layout.activity_loja)
+      setContentView(binding.root)
+      inicializar()
+   }
+
+   private fun inicializar() {
+      inicializarEventosClique()
+      inicializarObservaveis()
+   }
+
+   private fun inicializarEventosClique() {
+      with(binding){
+         btnLojaVoltar.setOnClickListener{
+            navegarPara(HomeActivity::class.java)
+         }
+         btnSelecionarImagemCapa.setOnClickListener {  }
+         btnSelecionarImagemPerfil.setOnClickListener {  }
+         btnAtualizar.setOnClickListener {  }
+      }
+   }
+
+   private fun inicializarObservaveis() {
+
    }
 }
